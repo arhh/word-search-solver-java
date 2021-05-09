1884aca65ae9e6b7469132ef183cc9db45fc728f
arhh <arhed7@yahoo.com>
2021-05-09 15:11:49 +0100

    Update row/column input fields to use JSpinner
    Easier for users to select row/column while preventing invalid input

--- src/main/java/arhh/wordsearchsolver/view/WordSearchView.java
+++ src/main/java/arhh/wordsearchsolver/view/WordSearchView.java
@@ -2,7 +2,7 @@ package arhh.wordsearchsolver.view;
 
 import arhh.wordsearchsolver.model.WordSearchModel;
 import arhh.wordsearchsolver.view.components.Button;
-import arhh.wordsearchsolver.view.components.FormattedTextField;
+import arhh.wordsearchsolver.view.components.NumberInput;
 import arhh.wordsearchsolver.view.components.TextField;
 
 import javax.swing.*;
@@ -37,8 +37,8 @@ public class WordSearchView extends JFrame implements ActionListener {
     private final JPanel appConfigPanel = new JPanel();
     private final JLabel rowInputLabel = new JLabel(ROW_INPUT_LABEL_TEXT);
     private final JLabel columnInputLabel = new JLabel(COLUMN_INPUT_LABEL_TEXT);
-    private final FormattedTextField rowInputField = FormattedTextField.createBoundedNumericalTextField(1, 15, 2);
-    private final FormattedTextField columnInputField = FormattedTextField.createBoundedNumericalTextField(1, 15, 2);
+    private final NumberInput rowInputField = NumberInput.createNumberInput(1, 15);
+    private final NumberInput columnInputField = NumberInput.createNumberInput(1, 15);
     private final Button applySetupButton = Button.createButton(APPLY_SETUP_BUTTON_TEXT, APPLY_SETUP_ACTION_COMMAND, this);
 
     // Word Search grid rendering
@@ -114,9 +114,9 @@ public class WordSearchView extends JFrame implements ActionListener {
         int rowCount;
         int columnCount;
         try {
-            rowCount = Integer.parseInt(rowInputField.getText());
-            columnCount = Integer.parseInt(columnInputField.getText());
-        } catch (NumberFormatException e) {
+            rowCount = (int) rowInputField.getValue();
+            columnCount = (int) columnInputField.getValue();
+        } catch (ClassCastException e) {
             rowCount = 1;
             columnCount = 1;
         }
