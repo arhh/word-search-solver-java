d9c8a217716b6e3fde06f547076a409e1c76652b
arhh <arhed7@yahoo.com>
2021-09-18 21:39:36 +0100

    Add JSON parser to Packager to allow reading from config JSON

--- src/main/java/arhh/wordsearchsolver/Packager.java
+++ src/main/java/arhh/wordsearchsolver/Packager.java
@@ -1,27 +1,38 @@
 package arhh.wordsearchsolver;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
+import java.io.*;
+import java.net.URL;
 import java.util.List;
-
 import com.badlogicgames.packr.Packr;
 import com.badlogicgames.packr.PackrConfig;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.compressors.CompressorException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Packager {
-    public static void main(String[] args) throws CompressorException, IOException, ArchiveException {
+    public static void main(String[] args) throws CompressorException, IOException, ArchiveException, ParseException {
+        JSONParser parser = new JSONParser();
+        URL is = Packager.class.getClassLoader().getResource("word-search-solver-linux.json");
+        JSONObject configJson = (JSONObject) parser.parse(new FileReader(is.getFile()));
+
         PackrConfig config = new PackrConfig();
-        config.platform = PackrConfig.Platform.Linux64;
-        config.executable = "word-search-solver-java";
-        config.classpath = List.of("target/word-search-solver-java-1.0-SNAPSHOT.jar");
-        config.mainClass = "arhh.wordsearchsolver.Main";
-        config.outDir = new File("./dist");
-        config.jdk = "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz";
+        for (PackrConfig.Platform platform : PackrConfig.Platform.values()) {
+            if (platform.toString().toLowerCase().equals((String) configJson.get("platform"))) {
+                config.platform = platform;
+                break;
+            }
+        }
+        config.executable = (String) configJson.get("executable");
+        config.classpath = (JSONArray) configJson.get("classpath");
+        config.mainClass = (String) configJson.get("mainclass");
+        config.outDir = new File((String) configJson.get("output"));
+        config.jdk = (String) configJson.get("jdk");
         config.vmArgs = List.of("");
         config.jrePath = "jre";
+
         new Packr().pack(config);
     }
 }
