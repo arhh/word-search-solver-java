5ba701b44ae5686b2204a8f01ba26c0d972179e1
arhh <arhed7@yahoo.com>
2021-09-19 21:03:17 +0100

    Swap json config with java Packager
    The Packager class is a self-contained class for generating distributions of the app for Linux, MacOS, and Windows

--- src/main/java/arhh/wordsearchsolver/Packager.java
+++ src/main/java/arhh/wordsearchsolver/Packager.java
@@ -1,38 +1,47 @@
 package arhh.wordsearchsolver;
 
 import java.io.*;
-import java.net.URL;
 import java.util.List;
 import com.badlogicgames.packr.Packr;
 import com.badlogicgames.packr.PackrConfig;
 import org.apache.commons.compress.archivers.ArchiveException;
 import org.apache.commons.compress.compressors.CompressorException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 public class Packager {
-    public static void main(String[] args) throws CompressorException, IOException, ArchiveException, ParseException {
-        JSONParser parser = new JSONParser();
-        URL is = Packager.class.getClassLoader().getResource("word-search-solver-linux.json");
-        JSONObject configJson = (JSONObject) parser.parse(new FileReader(is.getFile()));
+    public static void main(String[] args) throws CompressorException, IOException, ArchiveException {
+        PackrConfig[] packrConfigs = {generatePackrConfig(PackrConfig.Platform.Linux64), generatePackrConfig(PackrConfig.Platform.MacOS), generatePackrConfig(PackrConfig.Platform.Windows64)};
 
-        PackrConfig config = new PackrConfig();
-        for (PackrConfig.Platform platform : PackrConfig.Platform.values()) {
-            if (platform.toString().toLowerCase().equals((String) configJson.get("platform"))) {
-                config.platform = platform;
-                break;
-            }
+        for (PackrConfig packrConfig : packrConfigs) {
+            new Packr().pack(packrConfig);
         }
-        config.executable = (String) configJson.get("executable");
-        config.classpath = (JSONArray) configJson.get("classpath");
-        config.mainClass = (String) configJson.get("mainclass");
-        config.outDir = new File((String) configJson.get("output"));
-        config.jdk = (String) configJson.get("jdk");
+    }
+
+    private static PackrConfig generatePackrConfig(PackrConfig.Platform platform) {
+        PackrConfig config = new PackrConfig();
+        config.platform = platform;
+        config.executable = "word-search-solver-java";
+        config.classpath = List.of("./target/word-search-solver-java-1.0-SNAPSHOT.jar");
+        config.mainClass = "arhh.wordsearchsolver.Main";
+        config.outDir = new File(String.format("./dist/%s", platform));
+
+        switch (platform) {
+            case Linux64:
+                config.jdk = "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-linux-jdk.tar.gz";
+                break;
+            case MacOS:
+                config.jdk = "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.tar.gz";
+                break;
+            case Windows64:
+                config.jdk = "https://corretto.aws/downloads/latest/amazon-corretto-11-x64-windows-jdk.zip";
+                break;
+        }
+
+        // NullPointerException if these not set (as of Packr v4.0.0)
         config.vmArgs = List.of("");
         config.jrePath = "jre";
+        config.verbose = true;
+        config.cacheJre = new File("./cache");
 
-        new Packr().pack(config);
+        return config;
     }
 }
